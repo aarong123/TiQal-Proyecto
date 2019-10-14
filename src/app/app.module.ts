f4c569b207e38a05cb44c1ae480e2ef08afd7349
nicolas <nicolas.ortiz.a@hotmail.com>
2019-10-14 16:04:15 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { UserResolver } from './user/user.resolver';
+import { AuthGuard } from './core/auth.guard';
+import { AuthService } from './core/auth.service';
+import { UserService } from './core/user.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-//Rutas
+import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { CrearplantillaComponent } from './components/crearplantilla/crearplantilla.component';
 import { CriteriosComponent } from './components/criterios/criterios.component';
 import { HomeComponent } from './components/home/home.component';
 import { TablaComponent } from './components/tabla/tabla.component';
 import { TablaadminComponent } from './components/tablaadmin/tablaadmin.component';
-import { LoginComponent } from './components/login/login.component';
 import { PaginanoencontradaComponent } from './components/paginanoencontrada/paginanoencontrada.component';
-
-//Firebase
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
-
-//Servicios
-import { CriterioService } from './services/criterio/criterio.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    UserComponent,
+    RegisterComponent,
     MenuComponent,
     CrearplantillaComponent,
     CriteriosComponent,
@@ -35,18 +38,16 @@ import { CriterioService } from './services/criterio/criterio.service';
     TablaadminComponent,
     LoginComponent,
     PaginanoencontradaComponent,
-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,FormsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule // imports firebase/auth, only needed for auth features
   ],
-  providers: [CriterioService],
+  providers: [AuthService, UserService, UserResolver, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { UserResolver } from './user/user.resolver';
+import { AuthGuard } from './core/auth.guard';
+import { PaginanoencontradaComponent } from './components/paginanoencontrada/paginanoencontrada.component';
 import { CrearplantillaComponent } from './components/crearplantilla/crearplantilla.component';
 import { CriteriosComponent } from './components/criterios/criterios.component';
 import { HomeComponent } from './components/home/home.component';
 import { TablaComponent } from './components/tabla/tabla.component';
 import { TablaadminComponent } from './components/tablaadmin/tablaadmin.component';
-import { LoginComponent } from './components/login/login.component';
-import { PaginanoencontradaComponent } from './components/paginanoencontrada/paginanoencontrada.component';
 
-const routes: Routes = [
+export const rootRouterConfig: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: UserComponent,  resolve: { data: UserResolver}},
   { path: 'pagina-no-encontrada', component: PaginanoencontradaComponent},
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'plantilla', component: CrearplantillaComponent },
@@ -17,10 +25,5 @@ const routes: Routes = [
   { path: 'tabla', component:  TablaComponent },
   { path: 'tablaADMIN', component:  TablaadminComponent },
   { path: '**', redirectTo: '/pagina-no-encontrada', pathMatch: 'full' }
+  
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
